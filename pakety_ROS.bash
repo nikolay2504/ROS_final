842d10cf983f66098119f73ae19abac05ab0cab5
nikolay2504 <nikolay_filyushkin@mail.ru>
2025-02-07 14:54:25 +0300

    Update pakety_ROS.bash

--- pakety_ROS.bash
+++ pakety_ROS.bash
@@ -1,6 +1,6 @@
 apt update
 apt install -y ros-noetic-velodyne
-sudo apt install python3 python3-pip
+apt install python3 python3-pip
 pip3 install --upgrade odrive 
 git clone https://github.com/norlab-ulaval/norlab_xsens_driver/
 
